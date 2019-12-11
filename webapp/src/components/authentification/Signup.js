5d5844639194b2524ee463112d517b25bc86f8b3
Jess <jesszeroual@gmail.com>
2019-12-11 01:12:45 +0100

    add request POST

--- webapp/src/components/authentification/Signup.js
+++ webapp/src/components/authentification/Signup.js
@@ -3,12 +3,50 @@ import { Link } from 'react-router-dom';
 import { Form, Input, Button } from './Authentification.styled';
 
 const Signup = () => {
+  let username;
+  let email;
+  let password;
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+      }),
+    }).then(((username.value = ''), (email.value = ''), (password.value = '')));
+  };
+
   return (
     <>
-      <Form>
-        <Input type="text" placeholder="username" />
-        <Input type="email" placeholder="email" />
-        <Input type="password" placeholder="password" />
+      {/* TODO: validation */}
+      <Form onSubmit={e => handleSubmit(e)}>
+        <Input
+          ref={node => {
+            username = node;
+          }}
+          type="text"
+          placeholder="username"
+        />
+        <Input
+          ref={node => {
+            email = node;
+          }}
+          type="email"
+          placeholder="email"
+        />
+        <Input
+          ref={node => {
+            password = node;
+          }}
+          type="password"
+          placeholder="password"
+        />
         <Button>sign up</Button>
       </Form>
       <p>
