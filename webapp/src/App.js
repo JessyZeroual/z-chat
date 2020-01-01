fae8a576adddd260d24137a2eb89c002c06dda6f
Jess <jesszeroual@gmail.com>
2020-01-01 15:36:41 +0100

    conditional component rendering according to currentUser

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Authentification from './components/authentification/Authentification';
 import WorkSpace from './components/WorkSpace';
 
-const user = false;
-export default function App() {
-  return user ? <WorkSpace /> : <Authentification />;
-}
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getCurrentUser = async () => {
+    const response = await fetch('/api/getcurrentuser', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'GET',
+    });
+
+    if (response.ok) {
+      const user = await response.json();
+      setCurrentUser(user);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  if (isLoading) {
+    return 'Loading…';
+  }
+
+  return currentUser ? (
+    <WorkSpace currentUser={currentUser} />
+  ) : (
+    <Authentification getCurrentUser={getCurrentUser} />
+  );
+};
+
+export default App;
