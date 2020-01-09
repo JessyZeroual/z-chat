54c3e18f535325528e29fa677f6261511ef6779e
Jess <jesszeroual@gmail.com>
2020-01-09 23:10:26 +0100

    Add integration tests (API server with mocked data access layer)

---
+++ app.integration.spec.js
@@ -0,0 +1,46 @@
+const { when } = require('jest-when');
+const superTest = require('supertest');
+
+jest.mock('./data-access.js');
+const dataAccess = require('./data-access');
+const app = require('./app');
+
+const agent = superTest.agent(app);
+
+describe('app', () => {
+  describe('GET /api/channels', () => {
+    const channels = [
+      { id: '123', name: 'general' },
+      { id: '234', name: 'random' },
+    ];
+
+    beforeEach(() => {
+      dataAccess.getUserFromSessionId = jest.fn();
+      when(dataAccess.getUserFromSessionId)
+        .calledWith('a42be28b-0fb7-4d7f-9a49-7111defabcfd')
+        .mockReturnValue({
+          id: 1,
+          email: 'jess@gmail.com',
+          username: 'Jessy',
+        });
+      dataAccess.getAllChannels = jest.fn(() => channels);
+    });
+
+    describe('when user is authenticated', () => {
+      it('responds with channel list', async () => {
+        const response = await agent
+          .get('/api/channels')
+          .set('Cookie', 'sessionId=a42be28b-0fb7-4d7f-9a49-7111defabcfd');
+        expect(response.status).toEqual(200);
+        expect(JSON.parse(response.text)).toEqual({ channels });
+      });
+    });
+
+    describe('when user is not authenticated', () => {
+      it('responds with status 401', async () => {
+        const response = await agent.get('/api/channels');
+        expect(response.status).toEqual(401);
+      });
+    });
+  });
+});
---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const routes = require('./routes');
+const { setUser } = require('./middlewares');
+
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(setUser);
+
+app.use('/api', routes);
+app.use(express.static(path.join(__dirname, 'webapp', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'webapp', 'build', 'index.html'));
+});
+module.exports = app;
