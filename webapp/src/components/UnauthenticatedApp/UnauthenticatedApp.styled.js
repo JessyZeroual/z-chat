0bda3b4ba026f2e3713f1ce3b4a8fa1c15e57654
Jess <jesszeroual@gmail.com>
2020-03-19 15:45:17 +0100

    Responsive for the registration & connection form.

--- webapp/src/components/UnauthenticatedApp/UnauthenticatedApp.styled.js
+++ webapp/src/components/UnauthenticatedApp/UnauthenticatedApp.styled.js
@@ -14,7 +14,7 @@ export const Container = styled.div`
 export const Form = styled.form`
   width: 450px;
   border-radius: 10px;
-  padding: 10px;
+  padding: 20px;
   color: white;
   @media (max-width: 576px) {
     width: 100%;
@@ -46,4 +46,18 @@ export const Button = styled.button`
   border-style: solid;
   border-color: transparent;
   margin: 5px 0px;
+`;
+
+export const ButtonIcon = styled.button`
+  width: ${props => (props.btnSize ? props.btnSize : '35px')};
+  height: ${props => (props.btnSize ? props.btnSize : '35px')};
+  margin: ${props => (props.btnMarginX ? props.btnMarginX : '0px 5px')};
+  border-radius: ${props => (props.btnSquare ? '0%' : '50%')};
+  border: ${props => (props.btnBorder ? props.btnBorder : 'none')};
+  color: ${props => (props.btnColor ? props.btnColor : '#fff')};
+  background-color: ${props => props.btnBgColor && props.btnBgColor};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `;
